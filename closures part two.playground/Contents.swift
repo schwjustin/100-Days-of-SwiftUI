7e5e32a62f3adc06ebfcc41a1e5857e83be0ed9b
schwjustin <schw.justin@gmail.com>
2020-04-20 22:00:29 -0400

    Day 7

--- closures part two.playground/Contents.swift
+++ closures part two.playground/Contents.swift
@@ -159,4 +159,15 @@ goShopping(for: "shoes") { item in
     }
 }
 
-
+func visitPlaces() -> (String) -> Void {
+    var places = [String: Int]()
+    return {
+        places[$0, default: 0] += 1
+        let timesVisited = places[$0, default: 0]
+        print("Number of times I've visited \($0): \(timesVisited).")
+    }
+}
+let visit = visitPlaces()
+visit("London")
+visit("New York")
+visit("London")
