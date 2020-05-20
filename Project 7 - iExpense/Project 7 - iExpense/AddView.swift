97cb0196bdf04ecd986aa7d0fdc1a767cc1b20d0
schwjustin <schw.justin@gmail.com>
2020-05-20 18:07:31 -0400

    Project 7

---
+++ Project 7 - iExpense/Project 7 - iExpense/AddView.swift
@@ -0,0 +1,58 @@
+//
+//  AddView.swift
+//  Project 7 - iExpense
+//
+//  Created by Justin Schwartz on 5/20/20.
+//  Copyright © 2020 Justin Schwartz. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @ObservedObject var expenses: Expenses
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    @State private var showingAlert = false
+
+    static let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing: Button("Save") {
+                if let actualAmount = Int(self.amount) {
+                    let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                    self.expenses.items.append(item)
+                    self.presentationMode.wrappedValue.dismiss()
+                } else {
+                    self.showingAlert = true;
+                }
+            })
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Error"),
+                      message: Text("Please type a valid integer amount"),
+                      dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
