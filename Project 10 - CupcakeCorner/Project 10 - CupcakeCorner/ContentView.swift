ab3ac7f79b30230f3ed6a3527d3b49d01f7baf15
schwjustin <schw.justin@gmail.com>
2020-06-04 19:17:27 -0400

    Started Challenge 4 and Project 10

---
+++ Project 10 - CupcakeCorner/Project 10 - CupcakeCorner/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Project 10 - CupcakeCorner
+//
+//  Created by Justin Schwartz on 6/3/20.
+//  Copyright © 2020 Justin Schwartz. All rights reserved.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Justin"
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    func encode(to encoder:Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
